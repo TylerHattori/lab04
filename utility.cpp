c1a5775d103b0088ad49b2c795a3fdfe84792906
Tyler Hattori <63313357+TylerHattori@users.noreply.github.com>
2020-05-03 22:58:36 -0700

    Add files via upload

--- utility.cpp
+++ utility.cpp
@@ -21,8 +21,8 @@ bool isEven(int x) {
     return false;
 }
 bool isPrime(int x) { 
-  for (int i = 2; i < x; i++)
-    for (int j = 2; j < x; j++)
+  for (int i = 0; i < x; i++)
+    for (int j = 0; j < x; j++)
     {
         if (i*j == x)
           return false;
